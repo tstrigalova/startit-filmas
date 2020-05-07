2823ebd206debc132fd8b9bc25f6fdb9f3b034b8
tstrigalova <53894541+tstrigalova@users.noreply.github.com>
2020-05-07 23:43:39 +0300

    rrr

--- data.py
+++ data.py
@@ -3,6 +3,7 @@ import os
 import psycopg2
 from db import get_one, get_all
 
+<<<<<<< Updated upstream
 
 def varda_diena(vards):
     sql = "SELECT * FROM vardadienas WHERE vards=%s ORDER BY vards"
@@ -35,3 +36,29 @@ def statistika(menesis='visi'):
     else:
         c.execute('SELECT menesis, diena, count(vards) from vardadienas WHERE menesis=? GROUP BY menesis, diena ORDER BY menesis ASC, diena ASC', (menesis,))
     return c.fetchall()
+=======
+# Iegūstam DB informāciju no vides mainīgajiem
+# lai nebūtu jāglabā parole publiski pieejama
+# Vienkāršam testam var nomainīt šīs vērtības pret konkrētiem lielumiem
+
+ELEPHANT_HOST = "balarama.db.elephantsql.com"
+ELEPHANT_NAME = "kfdthoqa"
+ELEPHANT_PASSWORD = "l8NuOnY1jLFNtI-gHoYmw4-ytDiPPHob"
+
+
+def test_connection():
+    """Pārbauda pieslēgumu datubāzei
+    
+    Returns:
+        string -- tekstu ar datubāzes versiju
+    """
+    dsn = "host={} dbname={} user={} password={}".format(ELEPHANT_HOST, ELEPHANT_NAME, ELEPHANT_NAME, ELEPHANT_PASSWORD)
+    conn = psycopg2.connect(dsn)
+    cur = conn.cursor()
+    cur.execute("SELECT version();")
+    record = cur.fetchone()
+    result = "You are connected to - " + str(record)
+    cur.close()
+    conn.close()
+    return result
+>>>>>>> Stashed changes
